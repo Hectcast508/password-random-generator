f867090608c04d689ccf123737590787ac3a3966
Capwonderbread <hectcast10@yahoo.com>
2022-03-11 16:21:46 -0800

    added my array's

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-// Assignment code here
+// Assignment code here 
 var nembers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "p", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
-var specCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~", ]; 
+var specCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~", ];
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
