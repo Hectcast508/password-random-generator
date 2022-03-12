0bceaa14b11b19f4d301682fc0f431f820420a07
Capwonderbread <hectcast10@yahoo.com>
2022-03-12 12:58:55 -0800

    added is statement for if all is false then call function again

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 // Assignment code here 
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "p", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
-var specCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~", ];
+var specCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~",];
 
 function generatePassword() {
   var passLength = prompt("Choose a password length between 8 and 128 characters.");
@@ -11,12 +11,16 @@ function generatePassword() {
     return generatePassword();
   }
   var numbers = confirm("Would you like numbers in you password?");
-  
+
   var lowerLetters = confirm("Would you like lower case letters in your password?");
 
   var upperLetters = confirm("Would you like upper case letters in your password?");
-  
+
   var specCharacters = confirm("Would you like speacial characters in your paaword?");
+  if (numbers === false && lowerLetters === false && upperLetters === false && specCharacters === false) {
+    alert("Must select at least one option. Try again.");
+    return generatePassword();
+  }
 
   var passChoices = {
     passLength: passLength,
@@ -26,6 +30,8 @@ function generatePassword() {
     specCharacters: specCharacters,
   };
 
+  
+
   function randomCharacters(array) {
     var randomIndex = Math.floor(Math.random() * array.length);
     return array[randomIndex];
@@ -33,6 +39,8 @@ function generatePassword() {
 
   var endResult = [];
   var mostCharacters = [];
+
+  
 
   if (passChoices.numbers) {
     mostCharacters.concat(numbers);
@@ -56,8 +64,10 @@ function generatePassword() {
     endResult.push(randomCharacters(mostCharacters));
   }
   
+  return endResult.join("");
 
 }
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
